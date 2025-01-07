7c28632c2f732126e97c70b117e25e5b05665d86
VaibhavB <vaibhav.bedarkar98@gmail.com>
2025-01-07 23:05:01 +0530

    Math object

---
+++ src/component/js/ObjectAndArrays.js
@@ -0,0 +1,201 @@
+/**
+ * The Math object in JavaScript provides a collection of properties and methods for mathematical constants and functions.
+ * It is not a constructor, so all properties and methods are static and can be called directly on Math.
+ */
+
+/**
+ * Returns the absolute value of a number.
+ * @param {number} x - The number whose absolute value is to be returned.
+ * @returns {number} The absolute value of the given number.
+ */
+Math.abs = function(x) {};
+
+/**
+ * Returns the arccosine (in radians) of a number.
+ * @param {number} x - A number between -1 and 1.
+ * @returns {number} The arccosine of the given number.
+ */
+Math.acos = function(x) {};
+
+/**
+ * Returns the hyperbolic arccosine of a number.
+ * @param {number} x - A number greater than or equal to 1.
+ * @returns {number} The hyperbolic arccosine of the given number.
+ */
+Math.acosh = function(x) {};
+
+/**
+ * Returns the arcsine (in radians) of a number.
+ * @param {number} x - A number between -1 and 1.
+ * @returns {number} The arcsine of the given number.
+ */
+Math.asin = function(x) {};
+
+/**
+ * Returns the hyperbolic arcsine of a number.
+ * @param {number} x - A number.
+ * @returns {number} The hyperbolic arcsine of the given number.
+ */
+Math.asinh = function(x) {};
+
+/**
+ * Returns the arctangent (in radians) of a number.
+ * @param {number} x - A number.
+ * @returns {number} The arctangent of the given number.
+ */
+Math.atan = function(x) {};
+
+/**
+ * Returns the arctangent of the quotient of its arguments.
+ * @param {number} y - The y coordinate.
+ * @param {number} x - The x coordinate.
+ * @returns {number} The arctangent of the quotient of the arguments.
+ */
+Math.atan2 = function(y, x) {};
+
+/**
+ * Returns the hyperbolic arctangent of a number.
+ * @param {number} x - A number between -1 and 1.
+ * @returns {number} The hyperbolic arctangent of the given number.
+ */
+Math.atanh = function(x) {};
+
+/**
+ * Returns the cube root of a number.
+ * @param {number} x - A number.
+ * @returns {number} The cube root of the given number.
+ */
+Math.cbrt = function(x) {};
+
+/**
+ * Returns the smallest integer greater than or equal to a number.
+ * @param {number} x - A number.
+ * @returns {number} The smallest integer greater than or equal to the given number.
+ */
+Math.ceil = function(x) {};
+
+/**
+ * Returns the cosine of a number.
+ * @param {number} x - A number (in radians).
+ * @returns {number} The cosine of the given number.
+ */
+Math.cos = function(x) {};
+
+/**
+ * Returns the hyperbolic cosine of a number.
+ * @param {number} x - A number.
+ * @returns {number} The hyperbolic cosine of the given number.
+ */
+Math.cosh = function(x) {};
+
+/**
+ * Returns the value of a base expression taken to a specified power.
+ * @param {number} base - The base number.
+ * @param {number} exponent - The exponent used to raise the base.
+ * @returns {number} The base raised to the power of the exponent.
+ */
+Math.pow = function(base, exponent) {};
+
+/**
+ * Returns the square root of a number.
+ * @param {number} x - A number.
+ * @returns {number} The square root of the given number.
+ */
+Math.sqrt = function(x) {};
+
+/**
+ * Returns the tangent of a number.
+ * @param {number} x - A number (in radians).
+ * @returns {number} The tangent of the given number.
+ */
+Math.tan = function(x) {};
+
+/**
+ * Returns the hyperbolic tangent of a number.
+ * @param {number} x - A number.
+ * @returns {number} The hyperbolic tangent of the given number.
+ */
+Math.tanh = function(x) {};
+
+/**
+ * Returns the natural logarithm (base e) of a number.
+ * @param {number} x - A number.
+ * @returns {number} The natural logarithm of the given number.
+ */
+Math.log = function(x) {};
+
+/**
+ * Returns the base 10 logarithm of a number.
+ * @param {number} x - A number.
+ * @returns {number} The base 10 logarithm of the given number.
+ */
+Math.log10 = function(x) {};
+
+/**
+ * Returns the base 2 logarithm of a number.
+ * @param {number} x - A number.
+ * @returns {number} The base 2 logarithm of the given number.
+ */
+Math.log2 = function(x) {};
+
+/**
+ * Returns the natural logarithm (base e) of 1 + a number.
+ * @param {number} x - A number.
+ * @returns {number} The natural logarithm of 1 + the given number.
+ */
+Math.log1p = function(x) {};
+
+/**
+ * Returns the largest of zero or more numbers.
+ * @param {...number} values - Numbers to compare.
+ * @returns {number} The largest of the given numbers.
+ */
+Math.max = function(...values) {};
+
+/**
+ * Returns the smallest of zero or more numbers.
+ * @param {...number} values - Numbers to compare.
+ * @returns {number} The smallest of the given numbers.
+ */
+Math.min = function(...values) {};
+
+/**
+ * Returns a pseudo-random number between 0 (inclusive) and 1 (exclusive).
+ * @returns {number} A pseudo-random number between 0 and 1.
+ */
+Math.random = function() {};
+
+/**
+ * Returns the value of a number rounded to the nearest integer.
+ * @param {number} x - A number.
+ * @returns {number} The value of the given number rounded to the nearest integer.
+ */
+Math.round = function(x) {};
+
+/**
+ * Returns the sine of a number.
+ * @param {number} x - A number (in radians).
+ * @returns {number} The sine of the given number.
+ */
+Math.sin = function(x) {};
+
+/**
+ * Returns the hyperbolic sine of a number.
+ * @param {number} x - A number.
+ * @returns {number} The hyperbolic sine of the given number.
+ */
+Math.sinh = function(x) {};
+
+/**
+ * Returns the sign of a number, indicating whether the number is positive, negative or zero.
+ * @param {number} x - A number.
+ * @returns {number} 1 if the number is positive, -1 if the number is negative, 0 if the number is zero.
+ */
+Math.sign = function(x) {};
+
+/**
+ * Returns the integer part of a number by removing any fractional digits.
+ * @param {number} x - A number.
+ * @returns {number} The integer part of the given number.
+ */
+Math.trunc = function(x) {};
