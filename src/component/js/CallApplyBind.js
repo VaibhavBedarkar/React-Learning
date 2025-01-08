c783fb417fa7c943509b2d6d0669811adc4547b5
VaibhavB <vaibhav.bedarkar98@gmail.com>
2025-01-08 23:45:22 +0530

    [Call Apply Bind]

---
+++ src/component/js/CallApplyBind.js
@@ -0,0 +1,25 @@
+const person = {
+    firstName: "Vaibhav",
+    lastName: "Bedarkar",
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+const anotherPerson = {
+    firstName: "Viraj",
+    lastName: "Bedarkar"
+};
+
+// Using call
+// call allows us to use the method of one object in another object
+console.log(person.fullName.call(anotherPerson)); // Output: Viraj Bedarkar
+
+// Using apply
+// apply is similar to call, but it takes an array of arguments
+console.log(person.fullName.apply(anotherPerson)); // Output: Viraj Bedarkar
+
+// Using bind
+// bind creates a new function that, when called, has its 'this' keyword set to the provided value
+const boundFullName = person.fullName.bind(anotherPerson);
+console.log(boundFullName()); // Output: Viraj Bedarkar
